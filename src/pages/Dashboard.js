a64b9914be776b4ccc2d9ed593c0eef66e00d568
akeys-limbitless <a.keys@limbitless-solutions.org>
2025-11-26 14:21:26 -0500

    Initial commit: base React app with pages

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Placeholder for game history, start game button, leaderboard</p>
+    </div>
+  );
+}
