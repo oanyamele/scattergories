7a105010390f93e8ab1ceca87836737212977bef
akeys-limbitless <a.keys@limbitless-solutions.org>
2025-11-28 14:30:57 -0500

    Added const and import for buttons to work

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Dashboard() {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Dashboard</h1>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Login Page</h1>
