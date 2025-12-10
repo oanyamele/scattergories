e73aeef0599010a241270a4b3147c1c99d886e94
oanyamele <168555869+oanyamele@users.noreply.github.com>
2025-12-10 06:34:17 -0500

    Fix active round: remove glow from submit answers button to prevent flashing

--- src/pages/ActiveRound.tsx
+++ src/pages/ActiveRound.tsx
@@ -101,7 +101,6 @@ const ActiveRound: React.FC = () => {
             className="w-full"
             onClick={handleSubmit}
             disabled={isSubmitted}
-            glow
           >
             <Send className="w-5 h-5 mr-2" />
             Submit Answers
